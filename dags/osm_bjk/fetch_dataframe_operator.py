217cd77c325ba048a90a43e0f8cfdededee6e937
Jan Dalheimer <jan@dalheimer.de>
2023-09-30 10:19:10 +0200

    [dag/skolverket] Add DAG for skolenhetsregistret

--- dags/osm_bjk/fetch_dataframe_operator.py
+++ dags/osm_bjk/fetch_dataframe_operator.py
@@ -1,13 +1,50 @@
-from typing import Callable
+from datetime import datetime
+from typing import Callable, Iterable, cast
 
 import geopandas
 import numpy as np
+import psycopg2
 import ujson
 from airflow.models import BaseOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.utils.context import Context
 from geopandas import GeoDataFrame
 from pandas import RangeIndex
+
+
+def get_or_create_dataset(cur: psycopg2._psycopg.cursor, provider: str, dataset: str, dataset_url: str, license: str) -> int:
+    cur.execute(
+        "SELECT id FROM upstream.provider WHERE name = %s", (provider,)
+    )
+    provider_id = cur.fetchone()[0]
+    cur.execute(
+        "INSERT INTO upstream.dataset (name, provider_id, url, license) VALUES (%s, %s, %s, %s) ON CONFLICT (provider_id, name) DO UPDATE SET url = EXCLUDED.url, license = EXCLUDED.license RETURNING id",
+        (dataset, provider_id, dataset_url, license))
+    return cur.fetchone()[0]
+
+
+def upsert(cur: psycopg2._psycopg.cursor, items: Iterable[tuple[int, str, bytes, int, dict, datetime]]):
+    cur.executemany(
+        """
+WITH v (dataset_id, original_id, geometry, original_attributes, fetched_at) AS (
+    VALUES (%s, %s, ST_Transform(ST_Force2D(ST_GeomFromWKB(%s, %s)), 3006), %s::json, %s)
+),
+updated AS (
+    UPDATE upstream.item i SET original_attributes = v.original_attributes, geometry = v.geometry, fetched_at = v.fetched_at
+    FROM v WHERE i.dataset_id = v.dataset_id AND i.original_id = v.original_id
+    RETURNING id
+),
+inserted AS (
+    INSERT INTO upstream.item (dataset_id, original_id, geometry, original_attributes, fetched_at)
+    (SELECT v.* FROM v
+    LEFT OUTER JOIN upstream.item i2 ON v.dataset_id = i2.dataset_id AND v.original_id = i2.original_id
+    WHERE i2.id IS NULL)
+    RETURNING id
+)
+SELECT * FROM updated UNION ALL SELECT * FROM inserted
+        """,
+        ((i[0], i[1], i[2], i[3], ujson.dumps(i[4]), i[5]) for i in items)
+    )
 
 
 class FetchDataframeOperator(BaseOperator):
@@ -22,6 +59,7 @@ class FetchDataframeOperator(BaseOperator):
 
     def execute(self, context: Context):
         print("Fetching...")
+        fetched_at = datetime.utcnow()
         if isinstance(self.fetch, str):
             df = geopandas.read_file(self.fetch)
             print(f"Got {len(df)} items from {self.fetch}")
@@ -35,14 +73,7 @@ class FetchDataframeOperator(BaseOperator):
             hook.set_autocommit(conn, False)
             try:
                 with conn.cursor() as cur:
-                    cur.execute(
-                        "SELECT id FROM upstream.provider WHERE name = %s", (self.provider,)
-                    )
-                    provider_id = cur.fetchone()[0]
-                    cur.execute(
-                        "INSERT INTO upstream.dataset (name, provider_id, url, license) VALUES (%s, %s, %s, %s) ON CONFLICT (provider_id, name) DO UPDATE SET url = EXCLUDED.url, license = EXCLUDED.license RETURNING id",
-                        (self.dataset, provider_id, self.dataset_url, self.license))
-                    dataset_id = cur.fetchone()[0]
+                    dataset_id = get_or_create_dataset(cur, self.provider, self.dataset, self.dataset_url, self.license)
 
                     if isinstance(df.index, RangeIndex):
                         cur.execute(
@@ -50,11 +81,12 @@ class FetchDataframeOperator(BaseOperator):
                             (dataset_id,))
                         print("Loading data into database...")
                         items = [(dataset_id, row[1].geometry.wkb, df.crs.to_epsg(),
-                                  ujson.dumps({k: v for k, v in row[1].items() if k != 'geometry'}))
+                                  ujson.dumps({k: v for k, v in row[1].items() if k != 'geometry'}),
+                                  fetched_at)
                                  for row in df.iterrows() if row[1].geometry is not None]
                         ignored_items = len([1 for row in df.iterrows() if row[1].geometry is None])
                         cur.executemany(
-                            "INSERT INTO upstream.item (dataset_id, geometry, original_attributes) VALUES (%s, ST_Transform(ST_Force2D(ST_GeomFromWKB(%s, %s)), 3006), %s::json) RETURNING 1",
+                            "INSERT INTO upstream.item (dataset_id, geometry, original_attributes, fetched_at) VALUES (%s, ST_Transform(ST_Force2D(ST_GeomFromWKB(%s, %s)), 3006), %s::json, %s) RETURNING 1",
                             items
                         )
                         print(f"Inserted {len(items)} items")
@@ -65,30 +97,11 @@ class FetchDataframeOperator(BaseOperator):
                             "DELETE FROM upstream.item WHERE dataset_id = %s AND original_id <> ANY(%s)",
                             (dataset_id, list(df.index)))
                         print("Loading data into database...")
-                        items = [(dataset_id, row[0], row[1].geometry.wkb, df.crs.to_epsg(),
-                                  ujson.dumps({k: v for k, v in row[1].items() if k != 'geometry'}))
+                        items = [(dataset_id, cast(str, row[0]), cast(bytes, row[1].geometry.wkb), cast(int, df.crs.to_epsg()),
+                                  {k: v for k, v in row[1].items() if k != 'geometry'},
+                                  fetched_at)
                                  for row in df.iterrows()]
-                        cur.executemany(
-                            """
-WITH v (dataset_id, original_id, geometry, original_attributes) AS (
-        VALUES (%s, %s, ST_Transform(ST_Force2D(ST_GeomFromWKB(%s, %s)), 3006), %s::json)
-        ),
-    updated AS (
-        UPDATE upstream.item i SET original_attributes = v.original_attributes, geometry = v.geometry
-        FROM v WHERE i.dataset_id = v.dataset_id AND i.original_id = v.original_id
-        RETURNING id
-        ),
-    inserted AS (
-        INSERT INTO upstream.item (dataset_id, original_id, geometry, original_attributes)
-        (SELECT v.* FROM v
-         LEFT OUTER JOIN upstream.item i2 ON v.dataset_id = i2.dataset_id AND v.original_id = i2.original_id
-         WHERE i2.id IS NULL)
-         RETURNING id
-        )
-SELECT * FROM updated UNION ALL SELECT * FROM inserted
-                            """,
-                            items
-                        )
+                        upsert(cur, items)
                         print(f"Inserted or updated of {len(items)} items")
 
                     conn.commit()
---
+++ dags/skolverket_skolenheter.py
@@ -0,0 +1,69 @@
+from datetime import datetime, timedelta
+from typing import cast
+
+import shapely
+import ujson
+from airflow import DAG, Dataset
+from airflow.decorators import task
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from requests import Session
+
+from osm_bjk.fetch_dataframe_operator import get_or_create_dataset, upsert
+from osm_bjk.licenses import CC0_1_0
+
+with DAG(
+    "skolverket-skolenheter",
+    description="Fetches schools from Skolverket",
+    schedule_interval=None,
+    start_date=datetime(2023, 9, 16, 21, 30),
+    catchup=False,
+    max_active_runs=1,
+        default_args=dict(
+            depends_on_past=False,
+            email=["jan@dalheimer.de"],
+            email_on_failure=True,
+            email_on_retry=False,
+            retries=1,
+            retry_delay=timedelta(minutes=5)
+        )
+):
+    @task(task_id="fetch", outlets=[Dataset(f"psql://upstream/skolverket/skolenhetsregistret")])
+    def fetch():
+        hook = PostgresHook(postgres_conn_id="PG_OSM")
+        with hook.get_conn() as conn:
+            hook.set_autocommit(conn, False)
+            try:
+                with conn.cursor() as cur:
+                    with Session() as http:
+                        fetched_at = datetime.utcnow()
+
+                        dataset_id = get_or_create_dataset(cur, "Skolverket", "Skolenhetsregistret", "https://www.skolverket.se/skolutveckling/skolenhetsregistret", CC0_1_0)
+
+                        cur.execute("SELECT MAX(fetched_at) FROM upstream.item WHERE dataset_id = %s", (dataset_id,))
+                        row = cur.fetchone()
+                        if row is None or row[0] is None:
+                            units = http.get("https://api.skolverket.se/skolenhetsregistret/v1/skolenhet").json()
+                            units = [http.get(f"https://api.skolverket.se/skolenhetsregistret/v1/skolenhet/{unit['Skolenhetskod']}").json()["SkolenhetInfo"] for unit in units["Skolenheter"]]
+                            cur.executemany(
+                                "INSERT INTO upstream.item (dataset_id, original_id, geometry, original_attributes, fetched_at) VALUES (%s, %s, ST_SetSRID(ST_MakePoint(%s, %s), 3006), %s, %s)",
+                                ((dataset_id, unit["Skolenhetskod"],
+                                  unit["Besoksadress"]["GeoData"]["Koordinat_SweRef_E"].replace(",", "."),
+                                  unit["Besoksadress"]["GeoData"]["Koordinat_SweRef_N"].replace(",", "."),
+                                  ujson.dumps(unit), fetched_at
+                                  ) for unit in units if unit["Besoksadress"]["GeoData"]["Koordinat_SweRef_E"])
+                            )
+                        else:
+                            changed = http.get(f"https://api.skolverket.se/skolenhetsregistret/v1/diff/skolenhet/{row[0].strftime('%Y%m%d')}")
+                            units = [http.get(f"https://api.skolverket.se/skolenhetsregistret/v1/skolenhet/{unit}").json()["SkolenhetsInfo"] for unit in changed["Skolenheter"]]
+                            upsert(cur, (
+                                (dataset_id, cast(str, unit["Skolenhetskod"]), cast(bytes, shapely.geometry.Point(
+                                    float(unit["Besoksadress"]["GeoData"]["Koordinat_SweRef_E"].replace(",", ".")),
+                                    float(unit["Besoksadress"]["GeoData"]["Koordinat_SweRef_E"].replace(",", "."))
+                                ).wkb), 3006, unit, fetched_at)
+                                for unit in units
+                            ))
+            except Exception:
+                conn.rollback()
+                raise
+
+    fetch_t = fetch()

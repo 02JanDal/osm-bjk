029e21819d609725ba069250387982f5055d206e
Jan Dalheimer <jan@dalheimer.de>
2023-10-21 23:25:48 +0200

    [dag/osm] Put way geometry on the way table, remove way_geom table

--- dags/osm_bjk/replication/build_geometries.py
+++ dags/osm_bjk/replication/build_geometries.py
@@ -17,44 +17,42 @@ ON CONFLICT ON CONSTRAINT id_uniq DO NOTHING
     print("    Total dirty elements:", cursor.fetchone()[0])
     print("  Building ways...")
     cursor.execute("""
-INSERT INTO osm.way_geom (id, timestamp, geom) 
-	SELECT way.id, GREATEST((way.meta).timestamp, (
-	    SELECT MAX((node.meta).timestamp)
-		FROM osm.way_node
-		INNER JOIN osm.node ON node.id = way_node.node_id
-		WHERE way_node.way_id = way.id
-	)) as timestamp, (
+UPDATE osm.way SET
+	geom = (
 		SELECT ST_MakeLine(node.geom ORDER BY way_node.sequence_order)
 		FROM osm.way_node
 		INNER JOIN osm.node ON node.id = way_node.node_id
 		WHERE way_node.way_id = way.id
-	) as geometry
-	FROM osm.dirty
-	INNER JOIN osm.way ON dirty.way_id = way.id
-ON CONFLICT (id) DO UPDATE SET geom      = EXCLUDED.geom,
-							   timestamp = EXCLUDED.timestamp
+	),
+	geom_timestamp = GREATEST((way.meta).timestamp, (
+	    SELECT MAX((node.meta).timestamp)
+		FROM osm.way_node
+		INNER JOIN osm.node ON node.id = way_node.node_id
+		WHERE way_node.way_id = way.id
+	))
+FROM osm.dirty WHERE dirty.way_id = way.id
 """)
     print("  Building areas from ways...")
     cursor.execute("""
 INSERT INTO osm.area (id, geom, way_id, timestamp)
-	SELECT way_geom.id, ST_Multi(ST_MakePolygon(way_geom.geom)), way_geom.id, way_geom.timestamp
+	SELECT way.id, ST_Multi(ST_MakePolygon(way.geom)), way.id, way.geom_timestamp
 	FROM osm.dirty
-	INNER JOIN osm.way_geom ON dirty.way_id = way_geom.id
-	WHERE ST_IsClosed(way_geom.geom) AND ST_NPoints(way_geom.geom) > 3
+	INNER JOIN osm.way ON dirty.way_id = way.id
+	WHERE ST_IsClosed(way.geom) AND ST_NPoints(way.geom) > 3
 ON CONFLICT (way_id) DO UPDATE SET geom = EXCLUDED.geom, timestamp = EXCLUDED.timestamp
 """)
     print("  Building areas from multipolygons...")
     # TODO: ST_BuildArea is efficient, but ignores role information, is that an issue?
     cursor.execute("""
 INSERT INTO osm.area (id, relation_id, geom, timestamp)
-	SELECT relation.id + 3600000000, relation.id, ST_BuildArea(ST_Collect(way_geom.geom)), MAX(way_geom.timestamp)
+	SELECT relation.id + 3600000000, relation.id, ST_BuildArea(ST_Collect(way.geom)), MAX(way.geom_timestamp)
 	FROM osm.dirty
 	INNER JOIN osm.relation ON dirty.relation_id = relation.id
 	INNER JOIN osm.relation_member_way ON relation.id = relation_member_way.relation_id
-	INNER JOIN osm.way_geom ON way_geom.id = relation_member_way.member_id
-	WHERE tags ->> 'type' = 'multipolygon'
+	INNER JOIN osm.way ON way.id = relation_member_way.member_id
+	WHERE relation.tags->>'type' = 'multipolygon'
 	GROUP BY relation.id
-	HAVING ST_BuildArea(ST_Collect(way_geom.geom)) IS NOT NULL
+	HAVING ST_BuildArea(ST_Collect(way.geom)) IS NOT NULL
 ON CONFLICT (relation_id) DO UPDATE SET geom = EXCLUDED.geom, timestamp = EXCLUDED.timestamp
             """)
     print("  Cleaning dirty table...")
@@ -64,26 +62,20 @@ ON CONFLICT (relation_id) DO UPDATE SET geom = EXCLUDED.geom, timestamp = EXCLUD
 def build_geometries(cursor: Cursor):
     print("  Building ways...")
     cursor.execute("""
-    WITH timestamps AS (SELECT way.id, GREATEST((way.meta).timestamp, MAX((node.meta).timestamp)) as timestamp
-                        FROM osm.way
-                                 INNER JOIN osm.way_node ON way.id = way_node.way_id
-                                 INNER JOIN osm.node ON way_node.node_id = node.id
-                        GROUP BY way.id),
-         dirty AS (SELECT timestamps.id, timestamps.timestamp
-                   FROM timestamps
-                            LEFT OUTER JOIN osm.way_geom ON way_geom.id = timestamps.id
-                   WHERE way_geom.timestamp IS NULL
-                      or way_geom.timestamp < timestamps.timestamp),
-         geometry AS (SELECT dirty.id, ST_MakeLine(node.geom ORDER BY way_node.sequence_order) as geometry, dirty.timestamp
-                      FROM dirty
-                               INNER JOIN osm.way_node ON way_node.way_id = dirty.id
-                               INNER JOIN osm.node ON way_node.node_id = node.id
-                      GROUP BY dirty.id, dirty.timestamp)
-    INSERT
-    INTO osm.way_geom (id, geom, timestamp) (SELECT * FROM geometry)
-    ON CONFLICT (id) DO UPDATE SET geom      = EXCLUDED.geom,
-                                   timestamp = EXCLUDED.timestamp
-            """)
+UPDATE osm.way SET
+	geom = (
+		SELECT ST_MakeLine(node.geom ORDER BY way_node.sequence_order)
+		FROM osm.way_node
+		INNER JOIN osm.node ON node.id = way_node.node_id
+		WHERE way_node.way_id = way.id
+	),
+	geom_timestamp = GREATEST((way.meta).timestamp, (
+	    SELECT MAX((node.meta).timestamp)
+		FROM osm.way_node
+		INNER JOIN osm.node ON node.id = way_node.node_id
+		WHERE way_node.way_id = way.id
+	))
+""")
     print("  Building areas from ways...")
     cursor.execute("""
     INSERT INTO osm.area (id, geom, way_id, timestamp) (

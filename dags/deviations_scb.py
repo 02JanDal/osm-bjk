826d9814c0ecccf7ea50cd7f75ae1b4e6dbd842e
Jan Dalheimer <jan@dalheimer.de>
2024-01-01 21:38:52 +0100

    [dag] Add deviations calculation DAG for SCB and Skolverket

---
+++ dags/deviations_scb.py
@@ -0,0 +1,26 @@
+from datetime import timedelta, datetime
+
+from airflow import DAG, Dataset
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+
+for dataset_id, view_name, dataset_name in [(110, "preschools_scb", "forskolor")]:
+    with DAG(
+        f"deviations-{view_name}",
+        description=f"Updates deviations based on v_deviations_{view_name}",
+        schedule=[Dataset(f"psql://upstream/scb/{dataset_name}")],
+        start_date=datetime(2024, 1, 1, 0, 0),
+        catchup=False,
+        max_active_runs=1,
+        default_args=dict(
+            depends_on_past=False,
+            email=["jan@dalheimer.de"],
+            email_on_failure=True,
+            email_on_retry=False,
+            retries=1,
+            retry_delay=timedelta(minutes=5),
+        ),
+        tags=["provider:SCB", "type:Deviations"],
+    ):
+        SQLExecuteQueryOperator(
+            task_id="deviations", conn_id="PG_OSM", sql=f"SELECT upstream.sync_deviations('{view_name}', {dataset_id})"
+        )
---
+++ dags/deviations_skolverket.py
@@ -0,0 +1,25 @@
+from datetime import timedelta, datetime
+
+from airflow import DAG, Dataset
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+
+with DAG(
+    "deviations-schools-skolverket",
+    description="Updates deviations based on v_deviations_schools_skolverket",
+    schedule=[Dataset(f"psql://upstream/skolverket/skolenhetsregistret")],
+    start_date=datetime(2024, 1, 1, 0, 0),
+    catchup=False,
+    max_active_runs=1,
+    default_args=dict(
+        depends_on_past=False,
+        email=["jan@dalheimer.de"],
+        email_on_failure=True,
+        email_on_retry=False,
+        retries=1,
+        retry_delay=timedelta(minutes=5),
+    ),
+    tags=["provider:Skolverket", "type:Deviations"],
+):
+    SQLExecuteQueryOperator(
+        task_id="deviations", conn_id="PG_OSM", sql="SELECT upstream.sync_deviations('schools_skolverket', 109)"
+    )

2a087fa0f60bcc053b9cbfbe12b36a9d1437faf5
Rasmus Précenth <rasmus@precenth.eu>
2024-01-21 13:04:56 +0100

    [database] don't replace an https url with an http one

---
+++ database/tests/test_new_tag_value.sql
@@ -0,0 +1,81 @@
+SELECT test(
+    'fallback behavior should be to replace if distinct',
+    (true, 'a')::new_tag_value_type,
+    new_tag_value(
+        'arandomtag',
+        'a',
+        'b'
+    )
+);
+
+SELECT test(
+    'fallback behavior should be to replace if distinct, even if new is null',
+    (true, null)::new_tag_value_type,
+    new_tag_value(
+        'arandomtag',
+        null,
+        'b'
+    )
+);
+
+-- contact:website comparisons
+SELECT test(
+    'contact:website with a distinct url should be replaced',
+    (true, 'http://a.example.com')::new_tag_value_type,
+    new_tag_value(
+        'contact:website',
+        'http://a.example.com',
+        'https://b.example.com'
+    )
+);
+
+SELECT test(
+    'contact:website with the same url should not be replaced',
+    (false, null)::new_tag_value_type,
+    new_tag_value(
+        'contact:website',
+        'https://a.example.com',
+        'https://a.example.com'
+    )
+);
+
+
+SELECT test(
+    'contact:website with the same url but without https should be ignored',
+    (false, null)::new_tag_value_type,
+    new_tag_value(
+        'contact:website',
+        'http://example.com',
+        'https://example.com'
+    )
+);
+
+SELECT test(
+    'website with the same url but without https should be ignored',
+    (false, null)::new_tag_value_type,
+    new_tag_value(
+        'website',
+        'http://example.com',
+        'https://example.com'
+    )
+);
+
+SELECT test(
+    'contact:website with the same url with https should be used',
+    (true, 'https://example.com')::new_tag_value_type,
+    new_tag_value(
+        'contact:website',
+        'https://example.com',
+        'http://example.com'
+    )
+);
+
+SELECT test(
+    'website with the same url with https should be used',
+    (true, 'https://example.com')::new_tag_value_type,
+    new_tag_value(
+        'website',
+        'https://example.com',
+        'http://example.com'
+    )
+);

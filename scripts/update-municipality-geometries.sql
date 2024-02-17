def314c04631d0804adf5e8b5589cd245701f5c6
Rasmus Précenth <rasmus@precenth.eu>
2024-02-17 10:09:02 +0100

    scripts: only update municipalities with geometry in the database
    Makes it possible to work with a partial database.

--- scripts/update-municipality-geometries.sql
+++ scripts/update-municipality-geometries.sql
@@ -1,5 +1,15 @@
-UPDATE api.municipality SET geom = (
-SELECT ST_SimplifyPreserveTopology(ST_BuildArea(ST_Collect(geom)), 10) FROM osm.relation rel
-INNER JOIN osm.relation_member_way rmw ON rmw.relation_id = rel.id
-INNER JOIN osm.way wg ON rmw.member_id = wg.id
-WHERE rel.tags->>'type' = 'boundary' AND rel.tags->>'admin_level' = '7' AND rel.tags->>'KNKOD' = code);
+WITH computed AS (
+  SELECT rel.tags->>'KNKOD' AS code,
+         ST_SimplifyPreserveTopology(ST_BuildArea(ST_Collect(wg.geom)), 10) AS geom
+  FROM osm.relation AS rel
+  INNER JOIN osm.relation_member_way rmw ON rmw.relation_id = rel.id
+  INNER JOIN osm.way wg ON rmw.member_id = wg.id
+  WHERE rel.tags->>'type' = 'boundary' AND
+        rel.tags->>'admin_level' = '7' AND
+        rel.tags->>'KNKOD'::text IS NOT NULL
+  GROUP BY code
+)
+UPDATE api.municipality
+SET geom = COALESCE(computed.geom, api.municipality.geom)
+FROM computed
+WHERE api.municipality.code = computed.code;

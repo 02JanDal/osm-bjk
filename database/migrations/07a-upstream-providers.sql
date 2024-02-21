1659b45f18b7eb25409891c64e8fd988c5e79093
Rasmus Précenth <rasmus@precenth.eu>
2024-02-21 20:34:20 +0100

    [dag] add DAGs for Uppsala kommun

--- database/migrations/07a-upstream-providers.sql
+++ database/migrations/07a-upstream-providers.sql
@@ -4,5 +4,6 @@ INSERT INTO upstream.provider OVERRIDING SYSTEM VALUE VALUES (3, 'SCB', 'https:/
 INSERT INTO upstream.provider OVERRIDING SYSTEM VALUE VALUES (4, 'Lantmäteriet', 'https://www.lantmateriet.se/sv/geodata/vara-produkter/') ON CONFLICT DO NOTHING;
 INSERT INTO upstream.provider OVERRIDING SYSTEM VALUE VALUES (5, 'Trafikverket', 'https://www.trafikverket.se/') ON CONFLICT DO NOTHING;
 INSERT INTO upstream.provider OVERRIDING SYSTEM VALUE VALUES (6, 'Länsstyrelserna', 'https://gis.lansstyrelsen.se/geodata/geodatakatalogen/') ON CONFLICT DO NOTHING;
+INSERT INTO upstream.provider OVERRIDING SYSTEM VALUE VALUES (7, 'Uppsala kommun', 'https://opendata.uppsala.se/') ON CONFLICT DO NOTHING;
 
-SELECT pg_catalog.setval('upstream.provider_id_seq', 6, true);
+SELECT pg_catalog.setval('upstream.provider_id_seq', 7, true);
